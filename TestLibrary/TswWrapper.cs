3a4a4eba60af5e6997bb344a7999501d72631cb2
Alex <adpape@gmail.com>
2021-09-08 12:06:34 +0100

    Added test class

--- TestLibrary/TswWrapper.cs
+++ TestLibrary/TswWrapper.cs
@@ -1,11 +1,13 @@
-﻿using Crestron.SimplSharpPro.UI;
+﻿using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.UI;
 
 namespace TestLibrary {
-	public class TswAbstraction {
+	public class TswWrapper {
 		public Tsw760 Touchpanel { get; set; }
 
-		public TswAbstraction() {
-			
+		public TswWrapper(CrestronControlSystem cs_) {
+			Touchpanel = new Tsw760(0x03, cs_);
+			Touchpanel.Register();
 		}
 	}
 }
